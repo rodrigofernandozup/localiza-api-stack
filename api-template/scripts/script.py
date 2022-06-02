ec334f4eadb8b7f3e9be09a6c8c03fac662dccd8
Ricardo Pires <ricardo.jose@zup.com.br>
2022-06-02 10:43:55 -0300

    atualização https e msg de erro

--- api-template/scripts/script.py
+++ api-template/scripts/script.py
@@ -2,14 +2,14 @@ from templateframework.metadata import Metadata
 import requests
 
 def run(metadata: Metadata = None):
-    url = "http://runtime-engine-api.runtime-controlplane.sbox.stackspot.com/apps/"
+    url = "https://runtime-engine-api.runtime-controlplane.sbox.stackspot.com/apps/"
     headers = {"Content-Type": "application/json"}
     data = {
         "name": f"{metadata.global_computed_inputs['project_name_formated']}",
     }
     response = requests.put(url, headers=headers, json=data)
     if(response.status_code != 201):
-        print(f"Error runtine-engine: {response.status_code}")
+        print(f"Error runtime-engine: {response.status_code}")
     
     metadata.global_inputs['app_runtime_id'] = response.json()['id']
 
